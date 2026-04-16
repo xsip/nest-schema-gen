308f3d3f23905dca2c17390f6290b7a48d9f2a35
xsip <xsip@pm.me>
2026-04-16 03:03:20 +0200

    Improves example

--- integration-tests/demo-nest-app/src/app.module.ts
+++ integration-tests/demo-nest-app/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI', ''),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
